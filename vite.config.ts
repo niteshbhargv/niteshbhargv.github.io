756f707ebadfcc5696cb5a9bb55f7aae4de1ebb4
Nitesh Bhargava <nbhargava@adobe.com>
2025-06-27 01:09:33 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    middlewareMode: false,
+    fs: {
+      strict: true,
+    },
+  },
+  build: {
+    modulePreload: true,
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
+  },
 })
